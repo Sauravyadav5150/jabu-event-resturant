43a7a502e4b7c57138db720766c85cb8955e5bfe
shyam1joshi <shyam1joshi@gmail.com>
2024-02-07 13:50:40 +0530

    Adding access Rights to make it easy

--- addons/library/__manifest__.py
+++ addons/library/__manifest__.py
@@ -6,5 +6,8 @@
     'summary': 'Learn and understand basics of odoo aned show me ',
     'installable': True, # this will show install button on apps 
     #  'data': [  "views/addCreateadby.xml"  ], #usuall some kind of master data  , views and so on 
-    'depends': ['base']  #''' modules which will get installed before this  one '''
+    "data": [
+        "security/ir.model.access.csv"
+    ],
+    'depends': ['base','base_setup']  #''' modules which will get installed before this  one '''
 }
