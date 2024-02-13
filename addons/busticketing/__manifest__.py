29a06fea5e442996d6a5c8e0d79bbaa81cda0d59
Saurav <sauravyadav5150@gmail.com>
2024-02-13 15:59:54 +0530

    .

--- addons/busticketing/__manifest__.py
+++ addons/busticketing/__manifest__.py
@@ -3,13 +3,14 @@
     'version': '0.1',
     'category': 'Bus',
     'sequence': 15,
-    'summary': 'this is bus ticketing system',
+    'summary': 'this is bus ticket booking system',
     'installable': True, # this will show install button on apps 
     #  'data': [  "views/addCreateadby.xml"  ], #usuall some kind of master data  , views and so on 
      "data": [
-       # "security/ir.model.access.csv",
+        "security/ir.model.access.csv",
         "views/bus_view.xml",
         "views/menus.xml"
     ],
     'depends': ['base','base_setup']  #''' modules which will get installed before this  one '''
 }
+
